66ac633324d6cf721bb07541b4a2c10e4567e254
chenxiao4 <junhongchen@rcc-dhcp-248-250.sr.unh.edu>
2014-05-12 01:10:17 -0400

    add save option

--- Exploratory_Data_Analysis/plot4.R
+++ Exploratory_Data_Analysis/plot4.R
@@ -2,14 +2,19 @@ source("plot2.R")
 source("plot3.R")
 
 
-plot.four <- function(){
+plot.four <- function(save = TRUE){
         
-        png("plot4.png",width = 480, height = 480)
+        if (save){
+                png("plot4.png",width = 480, height = 480)
+        }
         par(mfrow = c(2,2))
         plot.two(save = FALSE)  
         plot.two(save = FALSE,col = 5,xtitle = "datetime",ytitle = "Voltage")
         plot.three(save = FALSE,0,"transparent")
         plot.two(save = FALSE,col = 4,xtitle = "datetime",ytitle = "Gobal reactive power")
-        dev.off()
+        
+        if (save){
+                dev.off()
+        }
                
 }
